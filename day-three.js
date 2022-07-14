7f9304471327fd221a1ffa854d96e4ba20fe9070
Bridgette Lemus <bridgettelemus@Bridgettes-MacBook-Pro.local>
2022-07-14 10:40:16 -0400

    created homework folder

---
+++ day-three.js
@@ -0,0 +1,23 @@
+// 1. Write a function that adds two numbers together.
+// 2. Write a car object that has a modal and color.  
+// Create a property called year then console.log all of the objects properties individually.
+// 3. Write a function to get the first element in an array.
+
+function add(){
+    if (){
+        console.log()
+    } else console.log()
+}
+
+let sum = (x,y) => x + y;
+
+let car = {
+    model: 'Saab',
+    car: 'Black',
+}
+
+car.year = 2020
+console.log(car.model)
+console.log(car.color)
+console.log(car.year)
+console.log( `My car's model is ${car.model}, the color is, and the year is`)
---
+++ lab2.js
@@ -0,0 +1,8 @@
+let car = {
+    make: 'Mini Cooper',
+    color: 'Black',
+};
+
+car['Title Owner'] = 'Bridgette';
+
+console.log(car)
